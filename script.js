912ecc650430b11eaafe5cab540a91628d8e52e4
DanielPiede <danielpiede@gmail.com>
2023-06-27 18:15:36 +0900

    close the menu first

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Code that needs to be executed on DOMContentLoaded event
+    // This could be any additional initialization code
+});
+
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px"
+
 function togglemenu() {
-    var menuList = document.getElementById("menuList");
 
     if (menuList.style.maxHeight === "0px") {
         menuList.style.maxHeight = "130px";
@@ -8,7 +15,3 @@ function togglemenu() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Code that needs to be executed on DOMContentLoaded event
-    // This could be any additional initialization code
-});
