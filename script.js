a87255e38406493af536b8625738d1d9a733f086
DanielPiede <danielpiede@gmail.com>
2023-06-27 14:42:09 +0900

    update all changes

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
 function togglemenu() {
-    document.getElementById("menuList");
-    menuList.style.maxHeight = "0px";
-    if (menuList.style.maxHeight == "0px")
-        {
-            menuList.style.maxHeight = "130px"
-        }
-    else
-        {
-            menuList.style.maxHeight = "0px"
-        }
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "130px";
+    } else {
+        menuList.style.maxHeight = "0px";
+    }
 }
