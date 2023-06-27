4aa2b1a44ac8cec08b5bd7ddd83c7e65a29bbdc1
DanielPiede <danielpiede@gmail.com>
2023-06-27 18:09:30 +0900

    updated the menu

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var menuList = document.getElementById("menuList");
-menuList.style.maxHeight = "0px";
-
 function togglemenu() {
-    if (menuList.style.maxHeight == "0px") {
+    var menuList = document.getElementById("menuList");
+
+    if (menuList.style.maxHeight === "0px") {
         menuList.style.maxHeight = "130px";
     } else {
         menuList.style.maxHeight = "0px";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Code that needs to be executed on DOMContentLoaded event
+    // This could be any additional initialization code
+});
