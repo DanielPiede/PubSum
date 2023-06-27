430b3f54fe38647a62bd1bc0e45416ed55085ae4
DanielPiede <danielpiede@gmail.com>
2023-06-27 14:35:08 +0900

    change script

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-document.getElementById("menuList");
-menuList.style.maxHeight = "0px";
-
 function togglemenu() {
+    document.getElementById("menuList");
+    menuList.style.maxHeight = "0px";
     if (menuList.style.maxHeight == "0px")
         {
             menuList.style.maxHeight = "130px"
