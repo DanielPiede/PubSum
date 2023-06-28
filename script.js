b2bf0a26f1ad2c9f0e85dbced37153f88a63c25e
DanielPiede <danielpiede@gmail.com>
2023-06-28 15:27:36 +0900

    update

--- script.js
+++ script.js
@@ -28,41 +28,24 @@ function togglemenu() {
     }
 }
 
-function handleSubmit(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    // Get the value entered in the search input
-    const searchValue = document.getElementById('search').value;
-
-    // Make the API call with the search value
-    fetch(`https://8gi380k5nd.execute-api.ap-northeast-1.amazonaws.com/Dev?q=${searchValue}`)
-        .then(response => response.json())
-        .then(data => {
-            // Display the results on the page
-            const searchResults = document.getElementById('searchResults');
-            searchResults.innerHTML = ''; // Clear previous results
-
-        if (data && data.length > 0) {
-            data.forEach(result => {
-                const resultItem = document.createElement('p');
-                resultItem.textContent = result;
-                searchResults.appendChild(resultItem);
-            });
-        }   
-        else {
-            const noResults = document.createElement('p');
-            noResults.textContent = 'No results found.';
-            searchResults.appendChild(noResults);
-        }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            const errorMessage = document.createElement('p');
-            errorMessage.textContent = 'An error occurred while fetching results.';
-            searchResults.appendChild(errorMessage);
-        });
+    // callAPI function that takes the base and exponent numbers as parameters
+    var callAPI = (search)=>{
+        // instantiate a headers object
+        var myHeaders = new Headers();
+        // add content type header to object
+        myHeaders.append("Content-Type", "application/json");
+        // using built in JSON utility package turn object to string and store in a variable
+        var raw = JSON.stringify({"search": search});
+        // create a JSON object with parameters for API call and store in a variable
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        // make API call with parameters and use promises to get response
+        fetch("https://8gi380k5nd.execute-api.ap-northeast-1.amazonaws.com/Dev", requestOptions)
+        .then(response => response.text())
+        .then(result => alert(JSON.parse(result).body))
+        .catch(error => console.log('error', error));
     }
-
-    // Add an event listener to the form to handle submission
-    const searchForm = document.getElementById('searchForm');
-    searchForm.addEventListener('submit', handleSubmit);
