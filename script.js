bb87929f9086163ace6ec5e2aeeaa65c647e6af1
DanielPiede <danielpiede@gmail.com>
2023-06-28 15:13:59 +0900

    commit

--- script.js
+++ script.js
@@ -27,3 +27,42 @@ function togglemenu() {
         menuList.style.maxHeight = "0px";
     }
 }
+
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the value entered in the search input
+    const searchValue = document.getElementById('search').value;
+
+    // Make the API call with the search value
+    fetch(`https://8gi380k5nd.execute-api.ap-northeast-1.amazonaws.com/Dev?q=${searchValue}`)
+        .then(response => response.json())
+        .then(data => {
+            // Display the results on the page
+            const searchResults = document.getElementById('searchResults');
+            searchResults.innerHTML = ''; // Clear previous results
+
+        if (data && data.length > 0) {
+            data.forEach(result => {
+                const resultItem = document.createElement('p');
+                resultItem.textContent = result;
+                searchResults.appendChild(resultItem);
+            });
+        }   
+        else {
+            const noResults = document.createElement('p');
+            noResults.textContent = 'No results found.';
+            searchResults.appendChild(noResults);
+        }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const errorMessage = document.createElement('p');
+            errorMessage.textContent = 'An error occurred while fetching results.';
+            searchResults.appendChild(errorMessage);
+        });
+    }
+
+    // Add an event listener to the form to handle submission
+    const searchForm = document.getElementById('searchForm');
+    searchForm.addEventListener('submit', handleSubmit);
