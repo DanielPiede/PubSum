5c59a88fb187684d599cf669053c6fb54b178fc6
DanielPiede <danielpiede@gmail.com>
2023-06-29 15:22:52 +0900

    updating contents and loading icon

--- script.js
+++ script.js
@@ -45,11 +45,21 @@ function callAPI(event) {
         redirect: 'follow'
     };
 
+    // Create the loading icon element
+    var loadingIcon = document.createElement('img');
+    loadingIcon.src = 'icons/loading.gif'; // Replace 'path/to/loading.gif' with the actual path to your GIF file
+    loadingIcon.classList.add('loading-icon'); // Add a CSS class for styling the loading icon
+
+    // Append Loading Icon
+    
+    var searchResults = document.getElementById('search-results');
+    searchResults.innerHTML = '';
+    searchResults.appendChild(loadingIcon);
+
+
     fetch("https://8gi380k5nd.execute-api.ap-northeast-1.amazonaws.com/Dev", requestOptions)
         .then(response => response.json()) // Parse the response as JSON
         .then(result => {
-        var searchResults = document.getElementById('searchResults');
-        searchResults.innerHTML = ''; // Clear the search results container
 
         // Iterate through the papers and generate HTML elements dynamically
         result.body.papers.forEach(paper => {
@@ -62,8 +72,10 @@ function callAPI(event) {
             dateElement.textContent = paper.Date;
 
             // Create a heading element for the title
-            var titleElement = document.createElement('h3');
+            var titleElement = document.createElement('a');
             titleElement.textContent = paper.Title;
+            titleElement.href = "https://doi.org/" + paper.DOI;
+            titleElement.target = "_blank";
 
             // Append the date and title elements to the paper box
             paperBox.appendChild(dateElement);
@@ -72,6 +84,10 @@ function callAPI(event) {
             // Append the paper box to the search results container
             searchResults.appendChild(paperBox);
         });
+        // Remove the loading icon
+        searchResults.removeChild(loadingIcon);
     })
     .catch(error => console.log('error', error));
+
+
 }
