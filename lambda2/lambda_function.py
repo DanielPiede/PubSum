49de88a4c17ba8ac844458e85e6d624e042645f9
DanielPiede <danielpiede@gmail.com>
2023-07-03 15:44:18 +0900

    in-between commit

--- lambda2/lambda_function.py
+++ lambda2/lambda_function.py
@@ -1,41 +1,58 @@
-import textract
 import requests
+import random
+import PyPDF2
+import io
+
 
 def lambda_handler(event, context):
-    full_text_url = event['fullTextUrl']
-    
+    url = event['url']  # Assuming the URL is passed as an input in the event
+
+    userAgents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0",
+        "Mozilla/5.0 (Windows NT 10.0; rv:114.0) Gecko/20100101 Firefox/114.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
+    ]
+
+    headers = {"User-Agent": random.choice(userAgents)}
+
+    session = requests.Session()
+    session.headers.update(headers)
+
     try:
-        response = requests.get(full_text_url)
-        response.raise_for_status()  # Check for any request errors
+        response = session.get(url, stream=True)
+        response.raise_for_status()  # Raise an exception if the request was not successful
 
-        # Extract text from the PDF
-        text = textract.process(response.content, method='pdfminer').decode('utf-8')
+        pdf_content = response.content
 
-        # Prepare the JSON response
-        json_response = {
-            'statusCode': 200,
-            'body': {
-                'fullText': text.strip()
-            }
-        }
-    except requests.exceptions.RequestException as e:
-        print(f"Error retrieving full text for URL: {full_text_url}")
-        print(str(e))
-        # Prepare an error response
-        json_response = {
-            'statusCode': 500,
-            'body': {
-                'error': f"Error retrieving full text for URL: {full_text_url}"
-            }
-        }
-    except textract.exceptions.MissingFileError:
-        print(f"PDF file not found for URL: {full_text_url}")
-        # Prepare an error response
-        json_response = {
-            'statusCode': 404,
-            'body': {
-                'error': f"PDF file not found for URL: {full_text_url}"
-            }
-        }
-    
-    return json_response
+        # Create a file-like object from the PDF content
+        pdf_file = io.BytesIO(pdf_content)
+
+        # Parse the PDF content using PyPDF2
+        pdf = PyPDF2.PdfReader(pdf_file)
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+    except Exception as e:
+        print(f"Error occurred while processing PDF: {str(e)}")
+        text = "N/A"
+
+    return {
+        'statusCode': 200,
+        'fulltext': text
+    }
+
